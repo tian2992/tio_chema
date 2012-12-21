d0269c736dd704e6f9ea543fa9ff9cb2edcbc2e7
Wences Arana <aranax@debian.org.gt>
2012-12-21 17:20:50 -0600

    Adding db manager files

---
+++ user.py
@@ -0,0 +1,11 @@
+
+class user:
+    def __init__(self, nick, seen, command, last, perm, karma, passwd, lvl):
+        self.nick = nick
+        self.seen = seen
+        self.command = command
+        self.last = last
+        self.perm = perm
+        self.karma = karma
+        self.passwd = passwd
+        self.lvl = lvl
