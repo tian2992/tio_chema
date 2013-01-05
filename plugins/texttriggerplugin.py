7218fd74aa0d7ede3b11b321556989c6db650e8a
Sebastian Oliva <tian2992@gmail.com>
2013-01-05 13:18:25 -0600

    fixed circular texttrigger reference

--- plugins/texttriggerplugin.py
+++ plugins/texttriggerplugin.py
@@ -3,16 +3,5 @@ from ircplugin import IRCPlugin
 
 class TextTriggerPlugin(IRCPlugin):
 
-  def trigger():
-      doc = "The trigger property, should be a regex."
-      def fget(self):
-          return self.trigger
-      def fset(self, value):
-          self.trigger = value
-      def fdel(self):
-          del self.trigger
-      return locals()
-  trigger = property(**trigger())
-
   def execute(self, ircMsg, userRole, regex_group):
     raise NotImplementedError
