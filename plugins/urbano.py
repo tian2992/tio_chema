3ab5a46e576f2ce90c85157aef1b8d6b39d214d4
Wences Arana <aranax@debian.org.gt>
2013-07-24 16:50:31 -0600

    Adding beautiful soup in urbano plugin

--- plugins/urbano.py
+++ plugins/urbano.py
@@ -1,8 +1,8 @@
 import requests
 from plugins.baseactionplugin import BaseActionPlugin
 from ircmessage import IRCMessage
-import HTMLParser
 from plugins.lengua import _
+from bs4 import BeautifulSoup
 
 class Urbano(BaseActionPlugin):
   def __init_(self):
@@ -11,6 +11,7 @@ class Urbano(BaseActionPlugin):
   def execute(self, ircMsg, userRole, *args, **kwargs):
       user = ircMsg.user
       m = IRCMessage()
+      definiciones = []
 
       term = m.msg
       print 'el valor de term es' + term
@@ -21,39 +22,17 @@ class Urbano(BaseActionPlugin):
       f = requests.get( url, params=payload)
       data = f.text
 
-      link =  LinksParser()
-      link.feed(data)
-      m.msg = link.data[0]
+      soup = BeautifulSoup( data )
+      tag = soup.find_all('div', attrs={'class' : 'definition' } )
+
+      for tagita in tag:
+        if type(tagita.string) != NoneType :
+          definiciones.append( tagita.string.strip() )
+
+      respuesta = definiciones[ random.randint( 0, len( definiciones ) - 1 ) ]
+
+      m.msg = respuesta
       m.channel = ircMsg.channel
       m.user = user
       m.directed = True
       return m
-
-
-
-class LinksParser(HTMLParser.HTMLParser):
-  def __init__(self):
-    HTMLParser.HTMLParser.__init__(self)
-    self.recording = 0
-    self.data = []
-
-  def handle_starttag(self, tag, attributes):
-    if tag != 'div':
-      return
-    if self.recording:
-      self.recording += 1
-      return
-    for name, value in attributes:
-      if name == 'id' and value == 'definition':
-        break
-    else:
-      return
-    self.recording = 1
-
-  def handle_endtag(self, tag):
-    if tag == 'div' and self.recording:
-      self.recording -= 1
-
-  def handle_data(self, data):
-    if self.recording:
-      self.data.append(data)
