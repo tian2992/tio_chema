d68d2a0da49e22071d3a7c698c39b9a8b0428e18
Sebastian Oliva <tian2992@gmail.com>
2013-08-03 13:49:11 -0600

    basic cleanup of plugin preguntar

--- plugins/preguntar.py
+++ plugins/preguntar.py
@@ -7,6 +7,7 @@ import os, sys
 from plugins.baseactionplugin import BaseActionPlugin
 from ircmessage import IRCMessage
 from plugins.lengua import _
+import logging
 
 class Preguntar(BaseActionPlugin):
   def __init_(self):
@@ -17,22 +18,26 @@ class Preguntar(BaseActionPlugin):
     self.url = 'http://answers.yahooapis.com/AnswersService/V1/questionSearch'
     self.output = 'json'
 
-  def get_question( self, topic ):
-    self.appid = 'D7KT0yvV34Fwo1YDnWKdCQibOdWcK9VzBHjut.3Y8wDrlfZKSluzcGje3wkGw3eE4CAjf65aTZs3xLd8DszXSdtFrYlyUiQ-'
-    self.region = 'e1'
-    self.url = 'http://answers.yahooapis.com/AnswersService/V1/questionSearch'
-    self.output = 'json'
-
+  def get_question(self, topic):
     payload = { 'query' : topic, 'appid' :  self.appid, 'region' : self.region, 'output' : self.output }
-    f = requests.get( self.url, params=payload )
-    data = json.loads(f.text)
-    preguntas = data['all']
-    if len( preguntas ) == 0 :
-      return 'Ese termino cerote no tiene respuesta'.encode( 'utf8' )
+    f = requests.get(self.url, params=payload)
+    # Something bad happened.
+    if f.status_code != 200:
+      logging.debug("HTTP error in preguntar plugin.")
+      return "[http-error]"
 
-    question = random.choice( preguntas['questions'] )
-
-    return u'Pregunta: ' + question['Subject'] + ' | Respuesta ' + ''.join(question['ChosenAnswer'].split('\n'))[:250] + ' | Link: ' + question['Link']
+    try:
+      data = json.loads(f.text)
+      preguntas = data['all']
+      if len(preguntas) == 0 :
+        return u"Ese termino no tiene respuesta."
+      question = random.choice(preguntas['questions'])
+      return u"Pregunta: {0} | Respuesta: {1} | Link {2}".format(
+        question['Subject'],
+        ''.join(question['ChosenAnswer'].split('\n'))[:250],
+        question['Link'])
+    except:
+      return "Error en preguntar"
 
   def execute(self, ircMsg, userRole, *args, **kwargs):
       user = ircMsg.user
@@ -41,7 +46,7 @@ class Preguntar(BaseActionPlugin):
       message = ' '.join(ircMsg.msg.split())
       topic = re.sub('^!preguntar ', '', message)
 
-      respuesta = self.get_question( topic  )
+      respuesta = self.get_question(topic)
 
       m.msg = respuesta
       m.channel = ircMsg.channel
