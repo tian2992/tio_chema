f45ab5c88367417523cd74291f91ceb87b2e06ea
Sebastian Oliva <tian2992@gmail.com>
2013-08-03 17:01:10 -0600

    basic cleanup of temblor

--- plugins/temblor.py
+++ plugins/temblor.py
@@ -2,41 +2,56 @@ import requests
 import types
 import re
 import json
+import logging
+import random
 from datetime import datetime
-from random import randint
 from plugins.baseactionplugin import BaseActionPlugin
 from ircmessage import IRCMessage
 from plugins.lengua import _
 from bs4 import BeautifulSoup
 
 class Temblor(BaseActionPlugin):
-  def __init_(self):
+  def __init__(self):
     BaseActionPlugin.__init__(self)
     self.synchronous = False
+    self.base_url = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson"
 
   def execute(self, ircMsg, userRole, *args, **kwargs):
       user = ircMsg.user
       m = IRCMessage()
       definiciones = []
-      message = ' '.join(ircMsg.msg.split())
-      term = re.sub('^!temblor ', '', message)
-      url = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson"
-      f = requests.get( url )
-      data = json.loads(f.text)
+      term = ircMsg.arguments
+      f = requests.get(self.base_url)
+      data = f.json()
 
-      for feature in data['features']:
-        cadena = feature['properties']['place']
-        
-        if cadena.upper().find( term.upper() ) != -1 :
-          definiciones.append( feature )
+      ## If there was a selected place.
+      if term:
+        logging.debug("Getting earthquakes for: '{0}'.".format(' '.join(term)))
+        for feature in data['features']:
+          cadena = feature['properties']['place']
+          if cadena.lower().find(' '.join(term).lower()) != -1 :
+            definiciones.append(feature)
+        ## In case there has not been any quake there recently.
+        if not definiciones:
+          ircMsg.msg = "There has not been any quake there in a while."
+          return ircMsg
+        temblo = random.choice(definiciones)
+      ## Let's just pick the most recent quake.
+      else:
+        temblo = data['features'][0]
 
-      size = len( definiciones )
-      temblo = definiciones[ randint( 0, size ) ]
-      fecha_t = datetime.fromtimestamp( temblo[ 'properties' ][ 'time' ] / 1000 )
-      fecha = fecha_t.strftime( '%d/%M/%Y %H:%m:%s')
-      
-      respuesta = '' + str( temblo[ 'properties' ][ 'mag' ] ) + ' | ' + temblo[ 'properties' ][ 'place' ] +  ' | ' + fecha + ' | '+ temblo[ 'properties' ][ 'url' ] 
-      m.msg = respuesta
+
+      ## Converting UNIX timestamp to human readable time.
+      date_t = datetime.fromtimestamp(temblo['properties']['time'] / 1000)
+      date = date_t.isoformat()
+
+      response_string = "Quake in: {0} | Magnitude: {1} | Time: {2} | URI: {3}".format(
+          temblo[ 'properties' ][ 'place' ],
+          temblo[ 'properties' ][ 'mag' ],
+          date,
+          temblo[ 'properties' ][ 'url' ]
+        )
+      m.msg = response_string
       m.channel = ircMsg.channel
       m.user = user
       m.directed = True
