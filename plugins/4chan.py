9c20207892f7dd1ce9dcc03c649b434e233e25fb
Wences Arana <aranax@debian.org.gt>
2013-08-10 17:04:17 -0600

    Fixing 4chan plugin

--- plugins/4chan.py
+++ plugins/4chan.py
@@ -3,6 +3,7 @@ import types
 import re
 import json
 import random
+import logging
 from plugins.baseactionplugin import BaseActionPlugin
 from ircmessage import IRCMessage
 from plugins.lengua import _
@@ -11,49 +12,83 @@ class CuatroChan(BaseActionPlugin):
   def __init__(self):
     BaseActionPlugin.__init__(self)
     self.synchronous = False
+    self.url = 'http://api.4chan.org/'
+    self.function_dict = {
+                          "default": self.get_random,
+                          "threads": self.get_threads,
+                         }
 
   def get_boards( self, abuscar ):
-      url = 'http://api.4chan.org/boards.json'
+      url = self.url + 'boards.json'
+      print url
       f = requests.get( url )
       data = json.loads(f.text)
 
-      for board in data['boards']:
-        if board['board'] == abuscar :
+      for board in data[ 'boards' ]:
+        if board[ 'board' ] == abuscar :
           return True
 
       return False
 
-  def get_threads( self, board ):
-      url = 'http://api.4chan.org/' + board + '/threads.json'
-      f = requests.get( url )
-      data = json.loads(f.text)
+  def get_threads( self, ircMsg ):
+      message = ' '.join( ircMsg.msg.split() )      
+      board = ircMsg.msg.split(' ')[ 1 ]
+      try:
+        existe = self.get_boards( board )
+        respuesta = ''
+        if not existe :
+          ircMsg.msg = 'Ese board mierda no existe'
+          return ircMsg
 
-      page = random.choice( data )
-      threads = random.choice( page['threads'] )
-      id_thread = threads['no']
+        respuesta = self.get_threads( board )
+        url = self.url + board + '/threads.json'    
+        f = requests.get( url )
+        data = json.loads( f.text )
+        page = random.choice( data )
+        threads = random.choice( page[ 'threads' ] )
+        id_thread = threads[ 'no' ]
+        ircMsg.msg = 'Te recomiendo este thread http://4chan.org/s/{thread}'.format( thread=id_thread )
+      except:
+        ircMsg.msg = 'Fallo el api 4chanesco'
+      return ircMsg
+
+  def get_random( self, ircMsg ):
+      url = self.url +  'b/threads.json'
+
+      try:
+        ircMsg.m = ''
+        f = requests.get( url )
+
+        data = json.loads( f.text )
+        page = random.choice( data )        
+        threads = random.choice( page['threads'] )
+        id_thread = threads['no']
+        url_resp = 'Te recomiendo este thread http://4chan.org/b/{0}'.format( id_thread )
+        ircMsg.m = url_resp
+      except:
+        logging.exception( 'Fallo en obtener json' )
+        ircMsg.m =  'Fallo el api 4chanesco' 
       
-      url_resp = 'http://4chan.org/' + board + '/' + str(id_thread)
+      return ircMsg
 
-      return url_resp
 
   def execute(self, ircMsg, userRole, *args, **kwargs):
       user = ircMsg.user
-      m = IRCMessage()
-      definiciones = []
-      message = ' '.join(ircMsg.msg.split())
-      board = re.sub('^!4chan ', '', message)
+      m = IRCMessage()      
+      message = ' '.join( ircMsg.msg.split() )
       
-      existe = self.get_boards( board )
-      respuesta = ''
-      if existe :
-        respuesta = self.get_threads( board )
-        respuesta = 'Te recomiendo este thread ' + respuesta
-      else :
-        respuesta = 'Ese board mierda no existe'
-
-      m.msg = respuesta
-      m.channel = ircMsg.channel
-      m.user = user
-      m.directed = True
-      return m
-
+      options = ircMsg.msg.split(' ')
+      try:
+        if( len( options ) > 1 ):
+          func = self.function_dict[ 'threads' ]
+          return func( ircMsg )
+        else: 
+          func = self.function_dict[ 'default' ]
+          a = func( ircMsg )
+          print a.m
+          return a.m
+      except:
+        logging.exception("Exception in 4chan command.")
+        ircMsg.msg = "Fracaso en el plugin 4chan"
+        return ircMsg
+    
