033cb054221fe2a0db5ff220fe127ac2be6b5856
Josue Ortega <josue@debian.org>
2016-09-18 18:17:36 -0600

    Adds plugin to read website titles

---
+++ plugins/webtitle.py
@@ -0,0 +1,30 @@
+import re
+import urllib2
+import logging
+
+from ircmessage import IRCMessage
+
+from plugins.texttriggerplugin import TextTriggerPlugin
+
+class WebTitle(TextTriggerPlugin):
+    def __init__(self):
+        self.trigger = re.compile(
+            """((http|https)://((?!twitter))\S*)""",
+            re.IGNORECASE
+        )
+
+    def get_title(self, url):
+        _title_re = re.compile("<title>(.+?)</title>")
+        _response = urllib2.urlopen(url)
+        content = _response.read()
+        return _title_re.search(content).group(1)
+
+    def execute(self, ircMsg, userRole, regex_groups):
+        try:
+            msg = IRCMessage()
+            msg.channel = ircMsg.channel
+            print(regex_groups)
+            msg.msg = self.get_title(regex_groups[0][0])
+        except:
+            logging.error(":(")
+        return msg
