5d6269771dd0562f8846c9b02d13000858dacf89
Sebastian Oliva <code@sebastianoliva.com>
2017-09-13 20:33:24 -0600

    added timestamp to irc messages

--- ircmessage.py
+++ ircmessage.py
@@ -1,3 +1,5 @@
+import datetime
+
 
 class IRCMessage():
   """Defines an IRC message."""
@@ -7,6 +9,7 @@ class IRCMessage():
     #if not channel.startswith("#"):
     #  self.channel = "#" + channel
     #else:
+    self.t = datetime.datetime.now()
     self.channel = channel
 
     self.msg = msg
