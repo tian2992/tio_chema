85bfed7085c0abb8296a8c6faff298b032b5957b
Sebastian Oliva <code@sebastianoliva.com>
2017-09-13 20:54:35 -0600

    moar

--- plugins/texttriggerplugin.py
+++ plugins/texttriggerplugin.py
@@ -5,6 +5,7 @@ class TextTriggerPlugin(IRCPlugin):
 
   def __init__(self):
     IRCPlugin.__init__(self)
+    self.trigger = None
 
   def execute(self, ircMsg, userRole, regex_group, *args, **kwargs):
     raise NotImplementedError
